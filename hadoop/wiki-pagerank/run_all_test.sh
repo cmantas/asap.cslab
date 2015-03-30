c9c3b547cf8baf4216df2805dbbd669e2686867b
asap Puppet-managed User <asap@imr40.internetmemory.org>
2015-03-31 01:23:43 +0200

    code cleanup

--- hadoop/wiki-pagerank/run_all_test.sh
+++ hadoop/wiki-pagerank/run_all_test.sh
@@ -1,7 +1,7 @@
-iterations=10
+iterations=3
 
-hadoop jar target/*.jar Main input/wikipedia output/wiki_pagerank $iterations 2>/dev/null
+hadoop jar target/*.jar Main input/wikipedia output/wiki_pagerank $iterations 2> pagerank_stderr.out 
 echo "===== TAIL  ====="
-hcat output/wiki_pagerank/* | tail -n 3 
+hcat output/wiki_pagerank/part* | tail -n 3 
 echo "===== HEAD  ====="
-hcat output/wiki_pagerank/* | head -n 3 
+hcat output/wiki_pagerank/part* | head -n 3 
--- hadoop/wiki-pagerank/step2.sh
+++
@@ -1,3 +0,0 @@
-hdfs dfs -rm -r /tmp/wiki_pagerank
-hadoop jar target/*.jar PreProcessTask input/wikipedia /tmp/wiki_pagerank/initial 2>&1 2>&1 | grep ERROR:
-hdfs dfs -cat  /tmp/wiki_pagerank/initial/part-r-00000 | head -n 2
