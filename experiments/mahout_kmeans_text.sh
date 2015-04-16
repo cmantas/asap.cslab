07b833b9ec4363290705d943f3bb3b8b798dbdd3
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-04-16 18:52:57 +0300

    mahout minDF loop

--- experiments/mahout_kmeans_text.sh
+++ experiments/mahout_kmeans_text.sh
@@ -18,9 +18,9 @@ hdfs dfs -mkdir -p $hadoop_input
 tfidf_dir=/tmp/mahout_tfidf
 
 sqlite3 results.db "CREATE TABLE IF NOT EXISTS mahout_tfidf 
-(id INTEGER PRIMARY KEY AUTOINCREMENT, documents INTEGER, time INTEGER, max_df INTEGER, terms INTEGER, date TIMESTAMP);"
+(id INTEGER PRIMARY KEY AUTOINCREMENT, documents INTEGER, time INTEGER, minDF INTEGER, terms INTEGER, date TIMESTAMP);"
 sqlite3 results.db "CREATE TABLE IF NOT EXISTS mahout_kmeans_text 
-(id INTEGER PRIMARY KEY AUTOINCREMENT, documents INTEGER, k INTEGER, terms INTEGER, max_df INTEGER, time INTEGER, date TIMESTAMP);"
+(id INTEGER PRIMARY KEY AUTOINCREMENT, documents INTEGER, k INTEGER, dimensions INTEGER, time INTEGER, date TIMESTAMP);"
 
 for ((docs=min_documents; docs<=max_documents; docs+=documents_step)); do
 	#re-load the parameters on each iteration for live re-configuration
@@ -29,10 +29,10 @@ for ((docs=min_documents; docs<=max_documents; docs+=documents_step)); do
 	echo "[PREP] Loading $docs text files"
 	$(dirname $0)/../hadoop/mahout-kmeans/myText2seq.sh $input_dir $hadoop_input $docs >/dev/null
 	
-	for ((dfp=min_maxDFpercent; dfp<=max_maxDFpercent; dfp+=maxDFpercent_step)); do
-		
+	for (( minDF=max_minDF; minDF>=min_minDF; minDF-=minDF_step)); do
+
 		# TF/IDF
-		echo "[EXPERIMENT] TF-IDF on $docs documents with a $dfp maxDFpercentage"
+		echo -n "[EXPERIMENT] TF-IDF on $docs documents, minDF=$minDF: "
 		tstart
 		$(dirname $0)/../hadoop/mahout-kmeans/mahout_tfidf.sh $hadoop_input $tfidf_dir $dfp  $minDF &> $operator_out
 		time=$(ttime)
@@ -40,19 +40,19 @@ for ((docs=min_documents; docs<=max_documents; docs+=documents_step)); do
 		
 		# find the dimensions of the output
 		dimensions=$(hadoop jar ${TOOLS_JAR}  seqInfo  $tfidf_dir/dictionary.file-0 | grep Lenght: | awk '{ print $2 }')
-
-		sqlite3 results.db "INSERT INTO mahout_tfidf(documents, max_df, terms, time, date )
-		                    VALUES( $docs, $dfp, $dimensions, $time, CURRENT_TIMESTAMP);"
+		echo $dimensions features, $time sec
+		sqlite3 results.db "INSERT INTO mahout_tfidf(documents, minDF, terms, time, date )
+		                    VALUES( $docs, $minDF, $dimensions, $time, CURRENT_TIMESTAMP);"
 	
 		for((k=min_k; k<=max_k; k+=k_step)); do
-			echo "[EXPERIMENT] mahout K-means with K=$k"
+			echo -n "[EXPERIMENT] mahout K-means with K=$k: "
 			tstart
 			$(dirname $0)/../hadoop/mahout-kmeans/mahout_kmeans_text.sh $tfidf_dir $k $max_iterations &>$operator_out
 			time=$(ttime)
-			sqlite3 results.db "INSERT INTO mahout_kmeans_text(documents, k, terms, max_df, time, date )
-			                    VALUES( $docs,  $k, $dimensions,  $dfp, $time, CURRENT_TIMESTAMP);"
-
 			check $operator_out
+			echo $time sec
+			sqlite3 results.db "INSERT INTO mahout_kmeans_text(documents, k, dimensions, time, date )
+			                    VALUES( $docs,  $k, $dimensions,  $time, CURRENT_TIMESTAMP);"
 		done
 	done
 done
