22641ea776a7f7aa3f8271ed918c0bf7cea87b78
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-07-07 17:51:15 +0300

    movers monitoring, etc

--- hadoop/mahout-kmeans/mahout_tfidf.sh
+++ hadoop/mahout-kmeans/mahout_tfidf.sh
@@ -5,12 +5,14 @@ minTF=$3
 input=$1
 output=$2
 
+
+#	-Dmapred.child.ulimit=15728640 -Dmapred.child.java.opts=-Xmx5g \
+#	-Dmapred.map.tasks=4 \
+
 echo "[STEP 2/4] Sequence to Sparse $input, $output, minTF=$minTF"
  mahout seq2sparse \
     -i $input -ow --chunkSize $chunk\
     -o $output --maxDFSigma 3.0  --namedVector --minSupport $minTF\
-	-Dmapred.child.ulimit=15728640 -Dmapred.child.java.opts=-Xmx5g \
-	-Dmapred.map.tasks=4 \
 	&>step2.out
 check step2.out
 
