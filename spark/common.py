c2896f980a071e4d5f11e5b318638f833ca579ff
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-11-26 17:10:23 +0200

    spark scripts retreive hdfs master

--- spark/common.py
+++ spark/common.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env pyspark
 __author__ = 'cmantas'
-
+from os import popen
 
 from os import environ
 
-core_site = environ['HADOOP_HOME']+"/etc/hadoop/core-site.xml"
 
-with open(core_site) as f:
-    content = f.readlines()
 
-hdfs_master = "localhost"
+# core_site = environ['HADOOP_HOME']+"/etc/hadoop/core-site.xml"
+#
+# with open(core_site) as f:
+#     content = f.readlines()
+#
+# hdfs_master = "localhost"
+#
+# # find the HDFS master from the config file
+# for line in content:
+#     if "hdfs" not in line.lower():
+#         continue
 
-# find the HDFS master from the config file
-for line in content:
-    if "hdfs" not in line.lower():
-        continue
-    host_start = line.index("://")+3
-    line = line[host_start:]
-    host_end = line.index(":")
-    hdfs_master= line[:host_end]
+# recover hdfs master from hadoop command
+line = popen("hadoop org.apache.hadoop.conf.Configuration | grep fs.defaultFS").read()
+host_start = line.index("://")+3
+line = line[host_start:]
+host_end = line.index(":")
+hdfs_master= line[:host_end]
 
 
 def to_hdfs_url(fname):
