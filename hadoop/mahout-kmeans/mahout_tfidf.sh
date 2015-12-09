20ac6f373e0a6406a96e478c52fbcf7683efc943
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2015-12-09 18:58:43 +0200

    spark imr logistic regression notebook

--- hadoop/mahout-kmeans/mahout_tfidf.sh
+++ hadoop/mahout-kmeans/mahout_tfidf.sh
@@ -1,4 +1,6 @@
-source $(dirname $0)/common.sh
+#!/bin/bash
+# "." command is like source (I guess)
+. $(dirname $0)/common.sh
 
 minTF=$3
 
@@ -8,13 +10,15 @@ output=$2
 
 
 echo "[STEP 2/4] Sequence to Sparse $input, $output, minTF=$minTF"
- mahout seq2sparse \
-	-Dmapred.child.ulimit=15728640 -Dmapred.child.java.opts=-Xmx5g \
-	-Dmapred.map.tasks=10 \
-    -i $input -ow --chunkSize $chunk\
-    -o $output --maxDFSigma 3.0  --namedVector --minSupport $minTF\
-	&>step2.out
-check step2.out
+mahout seq2sparse $tfidf_env    -i $input -ow --chunkSize $chunk\
+    -o $output --maxDFSigma 3.0  --namedVector --minSupport $minTF
+#	&>step2.out
+
+exit_status=$?
+#check step2.out
+
+
+exit $exit_status
 
 #+UseConcMarkSweepGC
 #       -Dmapred.map.child.java.opts=-Xmxg4g \
