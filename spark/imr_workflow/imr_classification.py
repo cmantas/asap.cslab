161b3a0f638e341fe131df1bbcde7eb8d358931a
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2016-02-12 14:02:39 +0200

    Created LogRegression of IMR  workflow in Spark Created_classification.py which performs logistic regression training and classification on datasets of specific format. Example usage can be found in showcase.sh One can run python imr_classification.py -h for help on arguments.

---
+++ spark/imr_workflow/imr_classification.py
@@ -0,0 +1,211 @@
+from pyspark.mllib.classification import \
+    LogisticRegressionWithLBFGS, LogisticRegressionModel
+from imr_tools import *
+from pyspark import SparkContext
+from argparse import ArgumentParser
+
+# ==================== Global Vars ============================ #
+_INTERCEPT = True
+_REGULARIZATION = None
+
+
+# ====================  Helper Functions ====================== #
+
+def train_model(training_rdd, **kwargs):
+    """
+    Train a classifier model using  an rdd training dataset
+    :param training_rdd: the rdd of the training dataset
+    :param kwargs: additional key-value params for the training (if any)
+    :return:
+    """
+    return LogisticRegressionWithLBFGS.train(training_rdd,
+                                             regType=_REGULARIZATION,
+                                             intercept=_INTERCEPT,
+                                             **kwargs)
+
+
+def get_cli_args():
+    """
+    Defines the command-line arguments, parses the input and returns a
+    namespace object with the parameters given by the user
+    :return:
+    """
+    cli_parser = ArgumentParser(description='Classification on Spark')
+    cli_parser.add_argument("operation",
+                            help="the operation to run: 'train' or 'classify'")
+    cli_parser.add_argument("input",
+                            help="the input dataset (formatted as a tuple)")
+    cli_parser.add_argument("-m", '--model', required=True,
+                            help="a csv file holding the model weights")
+    cli_parser.add_argument("-o", '--output',
+                            help="the output location (in case of classify op)")
+    cli_parser.add_argument("-l", '--labels', required=True,
+                            help="a csv file holding all labels in dataset")
+    cli_parser.add_argument("-c", '--category', type=int, default=1,
+                            help="which category (label) to use [1-3]")
+    cli_parser.add_argument("-u", "--update", action='store_true',
+                            default=False, help="update a pre-existing model")
+    cli_parser.add_argument("-e", "--evaluate", action='store_true',
+                            default=False,
+                            help="cross-evaluate the model on 20%% of input")
+
+    return cli_parser.parse_args()
+
+
+def calculate_error(valid_rdd, model):
+    """
+    Calculate the error ratio of a given model on a given RDD dataset
+    :param valid_rdd: an RDD of LabeledPoints
+    :param model: a classification model
+    :return: a float in the [0-1] range
+    """
+    label_and_preds = valid_rdd.map(
+            lambda p: (p.label, model.predict(p.features))
+            )
+    erroneous = label_and_preds.filter(lambda (l, p): l != p)
+    return erroneous.count() / float(valid_rdd.count())
+
+
+# ====================  Spark Jobs ====================== #
+
+def perform_train_job(input_path, l_encoder, initial_weights=None, evaluate=False):
+    """
+    Trains a Linear Regression model and returns its weights
+    :param input_path: the input file-name (local or HDFS)
+    :param parser: The parser function to use for creating the labeled points
+    :param initial_weights: the initial LR model (weights) we will be enhancing
+    :param evaluate: Whether or not to cross-evaluate the model on a 20% split
+    :return: a list of model weights for a Logistic Regression Model
+    """
+
+    # ----------------  Start Spark Job ----------------  #
+    print("=============>  Train/Update Model  <=============")
+    if "sc" not in globals():  # init the spark context
+        print("---> Init. Spark Context")
+        sc = SparkContext(appName="Train Logistic Regression (IMR)")
+
+    raw_data = sc.textFile(input_path)  # the raw text input RDD
+
+    # parse the raw data to labeled points
+    # Create a parser function based on the category number
+    # and label encoder. It parses:  text_line --> LabeledPoint
+    parsing_mapper_func = get_encoding_parser(args.category, l_encoder)
+    all_data = raw_data.map(parsing_mapper_func)  # an RDD of labeled Points
+
+    if evaluate:
+        # split the dataset in training and validation sets
+        (training_data, validation_data) = all_data.randomSplit([0.8, 0.2])
+    else:
+        # use the whole dataset
+        training_data = all_data
+
+    if initial_weights is None:
+        print("---> Training Classification Model")
+    else:
+        print("---> Updating Classification Model")
+
+    # ----------------   Do the training ----------------  #
+    model = train_model(training_data, numClasses=len(l_encoder.classes_))
+    print("   > Done")
+
+    # calculate training error
+    print("---> Calculating Training Error")
+    error = calculate_error(training_data, model)
+    print("   > "+str(error))
+
+    # if evaluate is given, calculate the evaluation error too
+    if evaluate:
+        print("---> Calculating Evaluation Error")
+        error = calculate_error(validation_data, model)
+        print("   > "+str(error))
+
+    return model.weights
+
+
+def perform_classification_job(input_path, encoder, model_weights,
+                               output_path):
+    """
+    Classifies the entries of a dataset based on a model created from the
+    weights given as an input
+    :param input_path: the input (text) dataset location
+    :param parser: The parser function to use for getting the features
+    :param model_weights: The LR model weights
+
+    :param output_path: The output dataset location
+    :return: None
+    """
+
+    # ----------------   Start Spark Job ----------------  #
+    print("===============>     Classify     <===============")
+    if "sc" not in globals():  # init the spark context
+        print("---> Init. Spark Context")
+        sc = SparkContext(appName="Train Logistic Regression (IMR)")
+
+    raw_data = sc.textFile(input_path)  # the raw text input RDD
+
+    # the parser just needs to take the features from a text line
+    parsing_mapper_func = get_features_from_line
+
+    # an RDD of feature vectors
+    feature_entries = raw_data.map(parsing_mapper_func)
+
+    # hack to find out the number of features in the dataset
+    feature_count = len(feature_entries.first())
+
+    # ----------- Create a model from given weights ------------ #
+    print("---> Re-creating the Model")
+    model = LogisticRegressionModel(model_weights, _INTERCEPT,
+                                    feature_count, len(encoder.classes_))
+
+    def classifying_func(features):
+        """
+        A closure (function) using classify_line and the model and encoder
+         that are available in this scope
+        :param features:
+        :return:
+        """
+        return classify_line(features, model, encoder)
+
+    # ---------------- Do the classification ---------------- #
+    print("---> Classifying the Input")
+    labeled_entries = feature_entries.map(classifying_func)
+    print("   > Done")
+
+    # save the output as a text file
+    labeled_entries.saveAsTextFile(output_path)
+
+
+# =======================  MAIN ========================= #
+if __name__ == "__main__":
+
+    # get the command-line arguments
+    args = get_cli_args()
+
+    # create a label encoder from a local csv file that contains the set
+    #   of available labels for this category
+    l_encoder = create_encoders_from_label_csvs(args.labels)
+
+    # ---------------  Choose the Operation to perform =-------------- #
+    if args.operation.lower() == "train":
+        # load initial weights if it's an update operation
+        init_weights = load_model_weights(args.model) if args.update else None
+
+        # do the train job
+        model_weights = perform_train_job(args.input, l_encoder,
+                                          initial_weights=init_weights,
+                                          evaluate=args.evaluate)
+        # save the model weights as a csv file
+        save_model_weights(model_weights, args.model)
+
+    elif args.operation.lower() == "classify":
+        if not args.output:
+            raise Exception("for classify operation, an output needs to be"
+                            "specified")
+        weights= load_model_weights(args.model)
+
+        # do the classification job
+        perform_classification_job(args.input, l_encoder,
+                                   weights, args.output)
+    else:
+        print("I do not know operation: "+args.operation)
+
---
+++ spark/imr_workflow/imr_tools.py
@@ -0,0 +1,131 @@
+from ast import literal_eval
+
+
+def create_label_encoder(labels):
+    """
+    Creates a label encoder from a list of labels
+    :param labels: a list of integers
+    :return: a LabelEncoder object
+    """
+    from sklearn.preprocessing import LabelEncoder
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+    return encoder
+
+
+def get_labels_from_label_csv(file_name):
+    """
+    Reads a single-line csv file that contains labels and returns said labels
+    :param file_name: a csv file
+    :return: an iterable of integer labels
+    """
+    line = open(file_name).readlines()[0]
+    labels = literal_eval(line)
+    return labels
+
+
+def create_encoders_from_label_csvs(*csvs):
+    """
+    Creates one or more label encoders from one or more csv filenames
+    :param csvs: files with integer labels in a single line
+    :return:
+    """
+
+    def create_one(csv):
+        """
+        helper function creating label encoder from single csv
+        """
+        labels = get_labels_from_label_csv(csv)
+        l_encoder = create_label_encoder(labels)
+        return l_encoder
+
+    if len(csvs) > 1:
+        return map(create_one, csvs)
+    else:
+        return create_one(csvs[0])
+
+
+def get_features_from_line(line):
+    """
+    Given a text line it returns
+     a) only the last element of the tuple if the line is a tuple.
+        That element we assume to be a list of features.
+     b) the line's elements if the line is not a tuple
+    :param line:
+    :return:
+    """
+    from ast import literal_eval
+    entry = literal_eval(line)
+    return entry[-1] if isinstance(entry, tuple) else entry
+
+
+def line_to_labeled_point(line, category, label_encoder=None):
+    """
+    Creates a label point from a text line that is formated as a tuple
+    :param line: line of format (3, 2, 1, [3,4,4 ..]), where the first entries
+            in the tuple are labels, and the last entry a list of features
+    :param category: which one of the labels in the tuple to keep for the
+            labeled point (0 to 2 for imr dataset)
+    :param label_encoder: an optional LabelEncoder object for the label value
+    :return: a LabeledPoint
+    """
+    from ast import literal_eval
+    from pyspark.mllib.classification import LabeledPoint
+
+    entry = literal_eval(line)
+    label = label_encoder.transform(entry[category]) if label_encoder \
+        else entry[category]
+
+    features = entry[-1]  # the last element in tuple is the feature list
+
+    return LabeledPoint(label, features)  # return a new labelPoint
+
+
+def save_model_weights(weights, filepath):
+    """
+    Saves the 'weights' list in file 'filepath'
+    :param weights:
+    :param filepath:
+    :return:
+    """
+    with open(filepath, 'w+') as f:
+        f.write(str(weights))
+
+
+def load_model_weights(file_path):
+    """
+    Loads the weights of a model from a single-line csv file
+    :param file_path:
+    :return:
+    """
+    with open(file_path) as f:
+        return literal_eval(f.read())
+
+
+def get_encoding_parser(category_no, encoder):
+        """
+        Creates a closure that parses lines to LabeledPoints.
+        Uses the category number of 'label' to chose a category label
+        Also the 'encoder' to encode the categories to consecutive integers.
+        :param category_no: the category number to choose
+        :param encoder: a LabeledEncoder for the given label category
+        :return: a function parsing a text line
+        """
+        return lambda line: line_to_labeled_point(line, category_no,
+                                                  label_encoder=encoder)
+
+
+def classify_line(features, model, l_encoder=None):
+    """
+    Classifies the features based on the given model.
+    If a label encoder is specified, it reverses the encoding of the label
+    :param features: a vector of features
+    :param model: a Classification Model
+    :param l_encoder: a LabelEncoder
+    :return: a tuple of:    label, [feat1, feat2 ... featN]
+    """
+    encoded_prediction = model.predict(features)
+    prediction = l_encoder.inverse_transform(encoded_prediction) \
+        if l_encoder else encoded_prediction
+    return prediction, features
+
