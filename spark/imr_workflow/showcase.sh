8984eab8d41bc28a155b57db4bef238afdc3c348
master - cslab cluster <cmantas@cslab.ntua.gr>
2016-02-15 17:22:52 +0200

    minor fixes in showcase

--- spark/imr_workflow/showcase.sh
+++ spark/imr_workflow/showcase.sh
@@ -1,39 +1,43 @@
 #!/usr/bin/env bash
 
 csv_dataset=$1 # the input dataset
+
 category=1 # the category to use
-work_dir=/tmp/imr_logistic_regression
+# a csv file with the all thepossible category labels of this categoyr
+categories=labels_$category.csv 
 
-categories=$work_dir/labels_$category.csv
-## TODO ###
-w2v_output=~/Data/imr_sample/ # the output of the W2V vectorization
-class_output=$work_dir/classification_output
+work_dir=hdfs://master:9000/tmp/imr_workflow # dir of intermediate/output data
 
-model=$work_dir/lr_model.csv # the logistic regression model csv
+w2v_jar_path=~/bin/lib/imr_w2v_2.11-1.0.jar # path for the w2v jar file
+
+w2v_model=$work_dir/w2v_model_spark # the dir where the w2v model will be saved
+w2v_output=$work_dir/imr_sample/ # the output vectors of w2v
+
+model=$work_dir/lr_model.csv # the logistic regression model (csv)
+class_output=$work_dir/classification_output # the output of the classification
 
 
 # create/clean up dirs, etc
-rm -r $work_dir &>/dev/null
-mkdir -p $work_dir
+rm -r $work_dir &>/dev/null; mkdir -p $work_dir # empty work dir
+hdfs dfs -rm -r $w2v_model # delete previous w2v model
 
-### PREPROCESS ###
-# create a csv of all available labels 
-#(showcase only: this should be done offline)
-cat $csv_dataset  | awk  -F ';'  "{print \$$((category +1))}"  |  \
-	uniq | tr '\n' ',' \
-	> $categories
 
+### TRAIN W2V ###
+spark-submit $w2v_jar_path sm $csv_dataset $w2v_model
+
+### Vectorize with W2V ###
+spark-submit $w2v_jar_path sv $w2v_model $csv_dataset $w2v_output
 
 ###  TRAIN CLASSIFIER  ###
 # create an initial model 
-python imr_classification.py train $w2v_output \
+spark-submit imr_classification.py train $w2v_output \
 	--model  $model \
 	--labels $categories \
 	--category $category \
 	--evaluate #flag for cross-evaluation on 20% of the data
 
 # update a previous model 
-python imr_classification.py train $w2v_output \
+spark-submit imr_classification.py train $w2v_output \
 	--model  $model \
 	--labels $categories \
 	--category $category \
@@ -47,7 +51,7 @@ rm -r $class_output 2>/dev/null
 #### CLASSIFY ######
 
 # classify based on this model
- python imr_classification.py classify $work_dir/mock.csv\
+ spark-submit imr_classification.py classify $work_dir/mock.csv\
         --output $class_output \
         --model  $model \
         --labels $categories 
