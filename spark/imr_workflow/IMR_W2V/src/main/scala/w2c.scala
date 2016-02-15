006790c98a6a1f4f6cd734ae0b552e89774bbb34
gsvic <victorasgs@gmail.com>
2016-02-15 15:32:34 +0200

    updates

--- spark/imr_workflow/IMR_W2V/src/main/scala/w2c.scala
+++ spark/imr_workflow/IMR_W2V/src/main/scala/w2c.scala
@@ -2,37 +2,85 @@ import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import org.apache.spark.mllib.linalg
 import org.apache.spark.util.{Vector => SV}
-
+import org.apache.log4j._
 
 object W2C extends App{
-  val inputPath = args(0)
-  val outputPath = args(1)
-  val op = args(2)
+  /* Logger initialization */
+  val layout = new SimpleLayout
+  val appender = new FileAppender(layout, "/home/vic/imr.log", false)
+  val logger = LogManager.getLogger(W2C.getClass)
+
+  logger.addAppender(appender)
+  logger.setLevel(Level.ALL)
+  logger.info("Starting")
+
+  var inputPath = null.asInstanceOf[String]
+  var outputPath = null.asInstanceOf[String]
+  var corpus = null.asInstanceOf[String]
+  val op = args(0)
 
   val sparkConf = new SparkConf().setAppName("IMR Word2Vec Workflow")
   val sc = new SparkContext(sparkConf)
-
-  //Word2Vec model
   var model = null.asInstanceOf[Word2VecModel]
 
-  //Loading input documents
-  lazy val input = sc.textFile(inputPath)
-
-  //Splitting input documents on ';'
-  lazy val splitted = input.map(x =>x.split(";"))
-
-  //Mapping each document into a tuple of format: (id, label1, label2, label3, text)
-  lazy val docs = splitted.map{ x =>
-    (x(0), x(1), x(2), x(3), x(4))
+  op match {
+    case "sm" => {
+      this.inputPath = args(1)
+      this.corpus = args(1)
+      this.outputPath = args(2)
+      trainAndSaveModel
+      logger.info(s"Model saved at: ${outputPath}!")
+    }
+    case "sv" => {
+      this.inputPath = args(1)
+      this.corpus = args(2)
+      this.outputPath = args(3)
+      println("OK")
+      saveVectors
+      logger.info(s"Vectors saved at: ${outputPath}")
+    }
+    case _ => {
+      println("Invalid argument")
+      System.exit(0)
+    }
   }
 
-  //Splitting and transforming each document into an array of words
-  lazy val docs_seq = docs.map( x => (x._1, x._2, x._3, x._4, x._5.split(" ").toSeq ) )
+  def trainAndSaveModel: Unit ={
+    logger.info("Training Word2Vec model")
+    logger.info(s"Input: ${this.input}")
+    logger.info(s"Output: ${this.outputPath}")
 
-  /**
+    val w2c = new Word2Vec().setVectorSize(200)
+    this.model = w2c.fit(this.input.map(_._5))
+    this.model.save(sc, outputPath)
+  }
+
+  def saveVectors: Unit = {
+    logger.info("Vectorization using existing model")
+    logger.info(s"Loading model at ${this.inputPath}")
+    logger.info("Loading model..")
+    model = Word2VecModel.load(sc, this.inputPath)
+    logger.info("model loaded.")
+    vectorized.saveAsTextFile(this.outputPath)
+    logger.info("saved.")
+  }
+
+  //Loading input documents
+  lazy val input = {
+    logger.info(s"preprocessing, ${corpus}")
+    sc.textFile(corpus)
+      .map(x => x.split(";"))
+      .map(x => (x(0), x(1), x(2), x(3), x(4)))
+      .map(x => (x._1, x._2, x._3, x._4, x._5.split(" ").toSeq))
+  }
+
+
+  /**l
     * Vectorize each document - Transforming it into a [[linalg.Vector]]
     * */
-  lazy val vectorized = docs_seq.map(x => (x._1, x._2, x._3, x._4, vectorizeDocument(x._5)))
+  lazy val vectorized = input.map{ x =>
+      (x._1, x._2, x._3, x._4, vectorizeDocument(x._5))
+  }
 
   /**
     * Vectorize each word - Transforming it into a [[linalg.Vector]] using [[Word2VecModel.transform()]]
@@ -53,21 +101,5 @@ object W2C extends App{
     linalg.Vectors.dense(bv.elements)
   }
 
-  def trainAndSaveModel: Unit ={
-    val w2c = new Word2Vec().setVectorSize(200)
-    this.model = w2c.fit(this.docs_seq.map(_._5))
-    this.model.save(sc, outputPath)
-  }
 
-  def saveVectors: Unit = {
-    model = Word2VecModel.load(sc, inputPath)
-    vectorized.saveAsTextFile(args(1))
-  }
-
-
-  op match {
-    case "sm" => trainAndSaveModel
-    case "sv" => saveVectors
-    case _ => println("Invalid argument")
-  }
 }
