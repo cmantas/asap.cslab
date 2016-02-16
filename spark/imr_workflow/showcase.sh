167a379e68e7079025b9c2c3b9faedbe762de78e
Christos Mantas <cmantas@cslab.ece.ntua.gr>
2016-02-16 19:36:16 +0200

    update

--- spark/imr_workflow/showcase.sh
+++ spark/imr_workflow/showcase.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 csv_dataset=$1 # the input csv_dataset (an hdfs url)
-category=2 # the category to use
+category=1 # the category to use
 categories=labels.json  # a json file holding all possible labels for each category
 
 
 ##############  intermediate  data #########################3###
 work_dir=$(dirname $csv_dataset)/intermediate # dir of intermediate/output data (hdfs)
 w2v_model=$work_dir/w2v_model_spark # the dir where the w2v model will be saved (hdfs)
-w2v_output=$work_dir/imr_sample/ # the output vectors of w2v (hdfs)
+w2v_output=$work_dir/w2v_vectors/ # the output vectors of w2v (hdfs)
 lr_model=$work_dir/lr_model # the dir where the logistic regression model will be saved (hdfs)
 class_output=$work_dir/classification_output # the output of the classification
 
@@ -19,7 +19,7 @@ w2v_jar_path=~/bin/lib/imr_w2v_2.11-1.0.jar # path for the w2v jar file
 hdfs dfs -rm -r $work_dir 
 hdfs dfs -mkdir -p $work_dir # empty work dir
 
-### (1.1) TRAIN W2V ###
+## (1.1) TRAIN W2V ###
 spark-submit $w2v_jar_path sm $csv_dataset $w2v_model
 
 ### (1.2) Vectorize with W2V ###
@@ -47,7 +47,8 @@ spark-submit --py-files imr_tools.py imr_classification.py train $w2v_output \
  spark-submit --py-files imr_tools.py imr_classification.py classify $w2v_output \
         --output $class_output \
         --model  $lr_model \
-        --labels $categories 
+        --labels $categories \
+	--category $category
 
 
 #echo "================> Results <====================="
